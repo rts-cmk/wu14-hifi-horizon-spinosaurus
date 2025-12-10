9407d2b0a58d44cf3f6b95328998ce492fa8c210
Gingersmurlf <theodorenghoff@gmail.com>
2025-12-10 13:53:25 +0100

    nu har jeg færdiggjort json filen

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 import express from "express"
 import cors from "cors"
+import fs from "fs"
 
 const PORT = 6767
 
 const app = express()
 
 app.get("/", (req, res) => {
-    res.json({ msg: "server is wok" })
+    fs.readFile("data.json", "utf-8", (err, data) => {
+        if (err) {
+            return res.json({ error: "den kan ikke læse indholdet dawg" })
+        }
+        const superData = JSON.parse(data)
+        res.send(superData)
+    })
+    
 })
 
 

03093b72754a0f3cf16845be0814ffe094d06fbd
Matias Jørgensen <matjorrub@gmail.com>
2025-12-11 13:23:11 +0100

    feat: add Shop page and integrate product filtering; update App routing and enhance ProductCard design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router";
 import "./style/main.scss";
 
+// Routes 
 import Home from "./pages/Home";
-import NotFound from "./components/NotFound";
+import Shop from "./pages/Shop";
 import About from "./pages/About";
+import NotFound from "./components/NotFound";
 
 export default function App() {
   return (
     <div className="page__wrapper">
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/*" element={<NotFound />}></Route>
       </Routes>

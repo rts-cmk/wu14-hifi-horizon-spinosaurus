7398ad9401081b390bd8dc171ab4a05a5c19e59f
Gingersmurlf <theodorenghoff@gmail.com>
2025-12-12 13:00:01 +0100

    theo-dev

---
+++ src/loader.jsx
@@ -0,0 +1,5 @@
+export async function ProductDetailLoader({ params }) {
+  const URL = `http://localhost:6767/product/${params.id}`;
+  const results = await fetch(URL).then((res) => res.json());
+  return { product: results };
+}
--- src/pages/ProductsDetailed.jsx
+++ src/pages/ProductsDetailed.jsx
@@ -1,92 +1,161 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { useParams } from "react-router";
+import { useLoaderData } from "react-router";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { FaSliders } from "react-icons/fa6";
+
+import "./productsDetailed.scss";
 
 export default function ProductsDetails() {
-  const [products, setProducts] = useState([]);
-
   const [cart, setCart] = useState([]);
   const [addToCart, setAddToCart] = useState(1);
 
+  const [selected, setSelected] = useState("");
+
+  const [localCart, setLocalCart] = useState();
+  const { product: products } = useLoaderData();
+  const getStockStatus = (stock) => {
+    if (stock === 0) return "Out of stock";
+    if (stock >= 1 && stock <= 3) return "Few in stock";
+    return "In stock";
+  };
+
   console.log(cart);
 
-  const { id } = useParams();
-
-  const URL = `http://localhost:6767/product/${id}`;
+  const stockStatus = getStockStatus(products.stock);
 
   useEffect(() => {
-    fetch(URL)
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    const storedCart = localStorage.getItem("cart");
+    if (storedCart) {
+      setLocalCart(JSON.parse(storedCart));
+    }
   }, []);
 
   return (
-    <div className="productsDetails">
+    <>
       <Header />
-      <h1 className="productsDetails__title">product</h1>
-      <section className="productsDetails__section">
-        <figure className="productsDetails__section__figure">
-          <img src={products.image} alt="" />
-        </figure>
-        <div className="productsDetails__section__info">
-          <h2>{products.name}</h2>
-          <p>{products.description}</p>
-          <button>{products.color}</button>
-          <div className="productsDetails__section__info__pricing">
-            <p>{products.price}</p>
-            <p>{products.stock}</p>
-            <div className="productsDetails__section__info__pricing__stockValue"></div>
-          </div>
-          <button
-            onClick={() => {
-              if (addToCart <= 1) {
-                return;
-              }
-              setAddToCart(addToCart - 1);
-            }}
-          >
-            minus
-          </button>
-          <p>{addToCart}</p>
-          <button onClick={() => setAddToCart(addToCart + 1)}>plus</button>
-          <br />
-          <button
-            onClick={() => {
-              if (!products || !products.id) {
-                return alert("produktet kan ikke tilføjes til kurven");
-              }
-
-              if (addToCart <= 0 || products.stock < addToCart) {
-                return alert(
-                  "du kan ikke tilføje 0 til kurven eller overskride lagerbeholdningen"
+      <div className="productsDetails">
+        <h1 className="productsDetails__title">product</h1>
+        <section className="productsDetails__section">
+          <figure className="productsDetails__section__figure">
+            <img src={products.image} alt="" />
+          </figure>
+          <div className="productsDetails__section__info">
+            <button className="productCard__compare">
+              Compare &nbsp; <FaSliders className="adjust__icon" size={18} />
+            </button>
+            <h2 className="productsDetails__section__info__name">
+              {products.name}
+            </h2>
+            <p className="productsDetails__section__info__description">
+              {products.description}
+            </p>
+            <div className="productsDetails__section__info__color">
+              {products.color.map((color, index) => {
+                return (
+                  <button
+                    onClick={() => {
+                      setSelected(color);
+                    }}
+                    className="productsDetails__section__info__color__button"
+                  >
+                    <span
+                      key={index}
+                      className="productsDetails__section__info__color__button__circle"
+                      style={{
+                        backgroundColor: color,
+                        border: selected == color ? "2px solid white" : "none",
+                        outline: selected == color ? `solid black 2px` : "none",
+                      }}
+                    ></span>
+                    {color}
+                  </button>
                 );
-              }
-
-              setCart((prevCart) => {
-                if (!Array.isArray(prevCart)) return [];
-
-                const existing = prevCart.find(
-                  (item) => item.id === products.id
-                );
-                if (existing) {
-                  if (existing.quantity + addToCart > products.stock) {
-                    return alert("du kan ikke overskride lagerbeholdningen");
+              })}
+            </div>
+            <div className="productsDetails__section__info__pricing">
+              <p className="productsDetails__section__info__pricing__price">
+                {products.price}
+              </p>
+              <p className="productsDetails__section__info__pricing__stock">
+                {stockStatus}
+                <span
+                  className={`stock__circle ${
+                    products.stock === 0
+                      ? "red"
+                      : products.stock <= 3
+                      ? "orange"
+                      : "green"
+                  }`}
+                ></span>
+              </p>
+            </div>
+            <div className="productsDetails__section__info__cart">
+              <button
+                onClick={() => {
+                  if (addToCart <= 1) {
+                    return;
                   }
-                  return prevCart.map((item) =>
-                    item.id === products.id
-                      ? { ...item, quantity: item.quantity + addToCart }
-                      : item
-                  );
-                }
-                return [...prevCart, { ...products, quantity: addToCart }];
-              });
-            }}
-            className="productsDetails__section__info__cartButton"
-          >
-            Add to cart
-          </button>
-        </div>
-      </section>
-    </div>
+                  setAddToCart(addToCart - 1);
+                }}
+              >
+                <FaMinus />
+              </button>
+              <p>{addToCart}</p>
+              <button onClick={() => setAddToCart(addToCart + 1)}>
+                <FaPlus />
+              </button>
+              <button
+                onClick={() => {
+                  if (!products || !products.id) {
+                    return alert("produktet kan ikke tilføjes til kurven");
+                  }
+
+                  if (addToCart <= 0 || products.stock < addToCart) {
+                    return alert(
+                      "du kan ikke tilføje 0 til kurven eller overskride lagerbeholdningen"
+                    );
+                  }
+
+                  setCart((prevCart) => {
+                    if (!Array.isArray(prevCart)) return [];
+
+                    const existing = prevCart.find(
+                      (item) => item.id === products.id
+                    );
+                    if (existing) {
+                      if (existing.quantity + addToCart > products.stock) {
+                        alert("du kan ikke overskride lagerbeholdningen");
+                        return prevCart;
+                      }
+                      return prevCart.map((item) =>
+                        item.id === products.id
+                          ? {
+                              ...item,
+                              quantity: item.quantity + addToCart,
+                              selectedColor: selected,
+                            }
+                          : item
+                      );
+                    }
+                    return [
+                      ...prevCart,
+                      {
+                        ...products,
+                        quantity: addToCart,
+                        selectedColor: selected,
+                      },
+                    ];
+                  });
+                }}
+                className="productsDetails__section__info__cart__cartButton"
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }

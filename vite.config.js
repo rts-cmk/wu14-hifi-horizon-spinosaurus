7bf647e3790db33c3b9aaadf99f72794f16307f8
Gingersmurlf <theodorenghoff@gmail.com>
2025-12-19 12:22:00 +0100

    lets see

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(() => {
       proxy: !isProd
         ? {
             "/api": {
-              target: "http://localhost:6767",
+              target: "https://hifi-server.vercel.app",
               changeOrigin: true,
               rewrite: (path) => path.replace(/^\/api/, ""),
             },
